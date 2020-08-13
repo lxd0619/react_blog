911e5e3fd0b1baec1ef199309c9680a0acb24afb
lixudong <lixudong@fulan.com.cn>
2020-08-13 10:44:13 +0800

    .

---
+++ docs/precache-manifest.70f19e81a06ddc16368179116065ffe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e763ec5acd1cb24a2523481a77be16da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f08e080200d5b8413ea",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "67bb25509ee662609b6b",
+    "url": "/static/js/2.0e100a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e100a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f08e080200d5b8413ea",
+    "url": "/static/js/main.305b76e9.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
