3e75120900c38f297df114d22a4d9cba17ee9e72
lixudong <lixudong@fulan.com.cn>
2020-11-10 11:52:43 +0800

    .

--- src/containers/Front_end/index.js
+++ src/containers/Front_end/index.js
@@ -1,76 +1,99 @@
 /* eslint-disable */
 import React, { Component } from "react";
+import { Tree } from "antd";
+const { DirectoryTree } = Tree;
 
 export class FrontEnd extends Component {
   state = {
     markdown: "",
+    treeData: [],
   };
 
   componentDidMount() {
+    this.getFileList();
+  }
+  getFileList() {
     const context = require.context("@/blogs", true, /\.md$/);
     const arr = context.keys();
-    console.log(context.id);
-    //匹配结果./js sync recursive \.js$
+    // console.log("context.id", context.id); //匹配结果./js sync recursive \.js$
+
     //参数分为三段，一、index.js当前路径，二、是否递归，三、正则匹配规则
     //context 关闭递归查找recursive 会变成 sync
-    console.log("arr", context.resolve(arr[0]));
+    // console.log("arr", context.resolve(arr[0]));
     //第一个js文件的相对路径./js/b.js
     console.log("keys()", context.keys());
     //所有成功匹配的js文件数组["./a.js", "./index.js", "./reducer/c.js", "./reducer/d.js"]
     const res = context.keys().map(context);
-    console.log("res", res);
+    // console.log("res", res);
 
     let list = [];
     let lists = [];
     context.keys().map((item) => {
-      // console.log("item", item);
-      // console.log("item", item.split(".")[1].split("/"));
-
-      let arr = item.split(".")[1].split("/");
-      console.log(arr.slice(1, arr.length));
-      lists.push({ ...arr.slice(1, arr.length) });
+      lists.push(item.split(".")[1].slice(1, item.split(".")[1].length));
 
       list.push({
-        title: item.split(".")[1].split("/").slice(-1),
+        title: item.split(".")[1].split("/").slice(-1)[0],
         path: context(item),
       });
     });
-    console.log(lists);
-    this.setState({ data: list });
+    this.setState({ list: list, treeData: this.listToTree(lists) });
   }
 
-  loop = (data) => {
-    let list = [];
-    data &&
-      data.map((item, index) => {
-        // console.log(Object.keys(item), index);
-        list.push({ title: item, children: [] });
-      });
-    // return list;
+  listToTree(arr) {
+    var tree = [];
+    if (Array.isArray(arr)) {
+      for (var i = 0; i < arr.length; ++i) {
+        var path = arr[i].split("/");
+        var _ret = tree;
+        for (var j = 0; j < path.length; ++j) {
+          var title = path[j];
+          var obj = null;
+          for (var k = 0; k < _ret.length; ++k) {
+            var _obj = _ret[k];
+            if (_obj.title === title) {
+              obj = _obj;
+              break;
+            }
+          }
+          if (!obj) {
+            obj = { title: title, key: title };
+            if (title.indexOf(".") < 0) obj.children = [];
+            _ret.push(obj);
+          }
+          if (obj.children) _ret = obj.children;
+        }
+      }
+    }
+    return tree;
+  }
+
+  onSelect = (keys, event) => {
+    console.log("Trigger Select", keys, event);
+    const { list } = this.state;
+    list.map((item) => {
+      item.title == keys[0] &&
+        this.props.history.push({
+          pathname: "/article",
+          query: { path: item.path },
+        });
+    });
+  };
+
+  onExpand = () => {
+    console.log("Trigger Expand");
   };
 
   render() {
-    const { data } = this.state;
+    const { list, treeData } = this.state;
+
     return (
       <div>
-        <ul>
-          {data &&
-            data.map((item, i) => {
-              return (
-                <li
-                  key={i}
-                  onClick={() => {
-                    this.props.history.push({
-                      pathname: "/article",
-                      query: { path: item.path },
-                    });
-                  }}
-                >
-                  {item.title}
-                </li>
-              );
-            })}
-        </ul>
+        <Tree
+          defaultExpandAll={true}
+          onSelect={this.onSelect}
+          onExpand={this.onExpand}
+          treeData={treeData}
+        />
       </div>
     );
   }
